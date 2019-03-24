dac74d483444b29e83b4867ff07865aaf96b601c
Matt McCoy <mdmccoy@gmail.com>
2019-03-24 15:01:10 -0400

    Change text to 420bot

--- 420bot.rb
+++ 420bot.rb
@@ -4,7 +4,7 @@ scheduler = Rufus::Scheduler.new
 # scheduler.cron '*/1 * * * *' do
 scheduler.cron '20 16 * * *' do
   client = Slack::Web::Client.new
-  client.chat_postMessage(channel: '#general', text: '421bot 420', as_user: true)
+  client.chat_postMessage(channel: '#general', text: '420bot 420', as_user: true)
 end
 
 class Four20Bot < SlackRubyBot::Bot
