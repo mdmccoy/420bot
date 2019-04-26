d5992e939d1d14ded53b6a95cacc475bc9f9350e
Matt McCoy <mdmccoy@gmail.com>
2019-04-26 09:20:29 -0400

    add-config-yaml - Change cron time back to 4:20 ruby time.

--- 420bot.rb
+++ 420bot.rb
@@ -1,7 +1,6 @@
 require './config.rb'
 
-SCHEDULER.cron '20 20 * * 1-5' do
-  # SystemTime execpted to be UTC, currently hardcoded to EST, M-F
+SCHEDULER.cron '20 16 * * 1-5' do
   client = Slack::Web::Client.new
   client.chat_postMessage(channel: CHANNEL, text: '420bot 420', as_user: true)
 end
