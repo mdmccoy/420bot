18d541fab594bedfefa6f0ff4871ee29ee6a9916
Matt McCoy <mdmccoy@gmail.com>
2019-04-26 09:18:28 -0400

    add-config-yaml - Use channel constant.

--- 420bot.rb
+++ 420bot.rb
@@ -3,7 +3,7 @@ require './config.rb'
 SCHEDULER.cron '20 20 * * 1-5' do
   # SystemTime execpted to be UTC, currently hardcoded to EST, M-F
   client = Slack::Web::Client.new
-  client.chat_postMessage(channel: '#dev_private', text: '420bot 420', as_user: true)
+  client.chat_postMessage(channel: CHANNEL, text: '420bot 420', as_user: true)
 end
 
 class Four20Bot < SlackRubyBot::Bot
