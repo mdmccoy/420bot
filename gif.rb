71187a89aa5dae438ba66bd01e785be08406ca9c
Matt McCoy <mdmccoy@gmail.com>
2019-05-22 19:36:17 -0400

    not-today - Refactor gif and add new command

--- gif.rb
+++ gif.rb
@@ -1,6 +1,5 @@
 module Gif
   require 'GiphyClient'
-  # require 'byebug'
 
   def self.fetch_gif(subject)
     api_instance = GiphyClient::DefaultApi.new
@@ -12,9 +11,10 @@ module Gif
     }
 
     begin
-      random_id = api_instance.gifs_random_get(ENV['GIPHY_API_KEY'], args).to_hash[:data][:id]
+      # make two calls here because the random endpoint doesn't return the downsized gif
+      random_id = api_instance.gifs_random_get(ENV['GIPHY_API_KEY'], args).to_hash.dig(:data,:id)
       result = api_instance.gifs_gif_id_get(ENV['GIPHY_API_KEY'], random_id)
-      result.to_hash[:data][:images][:downsized][:url]
+      result.to_hash.dig(:data,:images,:downsized,:url)
     rescue GiphyClient::ApiError => e
       puts "Exception when calling DefaultApi->gifs_random_get: #{e}"
     end
